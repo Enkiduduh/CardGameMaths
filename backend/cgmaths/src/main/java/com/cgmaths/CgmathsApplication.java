36a04390fb6a77ddc8defdc84c1c2b689f6c4572
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2025-08-24 13:38:53 +0200

    installation backend

---
+++ backend/cgmaths/src/main/java/com/cgmaths/CgmathsApplication.java
@@ -0,0 +1,13 @@
+package com.cgmaths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgmathsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgmathsApplication.class, args);
+	}
+
+}
---
+++ backend/cgmaths/src/test/java/com/cgmaths/CgmathsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgmaths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgmathsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
